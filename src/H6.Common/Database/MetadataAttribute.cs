c18f1fbfcaeed504adc11c05a5011d208803b256
Jiri Oulehla <oulehla.jiri@xacti.com>
2020-07-18 14:37:25 +0200

    add MetadataAttribute

---
+++ src/H6.Common/Database/MetadataAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace H6.Common.Database
+{
+  public class MetadataAttribute : Attribute
+  {
+    public MetadataAttribute()
+    {
+
+    }
+    public int Id { get; set; }
+  }
+}
