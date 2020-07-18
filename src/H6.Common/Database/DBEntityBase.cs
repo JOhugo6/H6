bfc7e4a3039c3852910572f66bb533a47b72044e
Jiri Oulehla <oulehla.jiri@xacti.com>
2020-07-18 14:14:31 +0200

    add DBEntityBase

---
+++ src/H6.Common/Database/DBEntityBase.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H6.Common.Database
+{
+  /// <summary>
+  /// Database entity
+  /// </summary>
+  /// <typeparam name="TEntity"></typeparam>
+  [Serializable]
+  public class DBEntityBase<TEntity>
+    where TEntity : class
+  {
+
+  }
+
+  /// <summary>
+  /// Database entity
+  /// </summary>
+  /// <typeparam name="TPrimaryKey"></typeparam>
+  /// <typeparam name="TEntity"></typeparam>
+  [Serializable]
+  public class DBEntityBase<TPrimaryKey, TEntity> : DBEntityBase<TEntity>, IPrimaryKeyAsId<TPrimaryKey>
+    where TEntity : DBEntityBase<TPrimaryKey, TEntity>
+    where TPrimaryKey : IComparable
+  {
+    #region Properties
+    /// <summary>
+    /// Id.
+    /// </summary>
+    public TPrimaryKey Id { get; set; }
+    #endregion
+  }
+}
---
+++ src/H6.Common/Database/IPrimaryKeyAsId.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H6.Common.Database
+{
+  public interface IPrimaryKeyAsId<TPrimaryKey> where TPrimaryKey : IComparable
+  {
+    #region Properties
+    /// <summary>
+    /// Id.
+    /// </summary>
+    TPrimaryKey Id { get; set; }
+    #endregion
+  }
+}
